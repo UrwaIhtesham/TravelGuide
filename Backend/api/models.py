52207d72fc1dbfcd6a5a8394b47584b2e7acc3ed
Urwa Ihtesham <urwaihtesham@gmail.com>
2025-03-15 08:42:18 +0500

    signup_functionality: added User model in backedn for signup

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+#User table model
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length = 15)
+    CNIC = models.CharField(max_length=25, unique=True, blank=True, null=True)
+    gender = models.CharField(max_length=10, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    password = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+# signup POST api
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message": "User created successfully", "user": serializer.data}, status = 201)
+    return Response(serializer.errors, status=400)
+        
