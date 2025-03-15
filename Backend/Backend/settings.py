3a62cd6cb9cd79e33dcbb08a16e7efa60a3dab6b
Urwa Ihtesham <urwaihtesham@gmail.com>
2025-03-15 08:08:45 +0500

    backend: initialized mySQL database in django

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -97,6 +97,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'root',
+        'PASSWORD': 'Password123*',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
